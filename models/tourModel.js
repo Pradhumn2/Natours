6a8e9d2c463fc572ed65962be70cb6dfcb0e3d4b
Pradhumn Sharma <84733033+Pradhumn2@users.noreply.github.com>
2023-08-26 13:23:11 +0530

    tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -120,53 +120,32 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
-//Indexing is use to sort the result as per required query, 
-//to avoid unnessacery scanning obj. while executing query
-//"USE ONLY FOR FREQUENTLY USED QUERIES BY USERS , COZ IT INCREASES TIME"
-tourSchema.index({price: 1, ratingsAverage: -1 , slug: -1});   //1: ascending order, -1: desending order
+tourSchema.index({price: 1, ratingsAverage: -1 , slug: -1});   
 
-tourSchema.index({tour: 1, user: 1} , {unique: true});   //For allowing only unique review of any tour by a particulat user.
+tourSchema.index({tour: 1, user: 1} , {unique: true});  
 
-tourSchema.index({ startLocation: '2dsphere'});  // startLocation should be a indexed to a 2dsphere(for real coordinates , like on earth)
+tourSchema.index({ startLocation: '2dsphere'});
 
 tourSchema.virtual("durationWeeks").get(function () {
-  //>> not a part of db
   return this.duration / 7;
 });
 
-//Virtual Populate (To show reviews in particular tour, we couldn't do normal embedding
-//cuz tour to review is P->C referencing and we can't access parent(tour) in reviews)
-//            {name of virtual entity}
 tourSchema.virtual('reviews', {
   ref: 'Review',
   foreignField: 'tour', // {foregin key for Review which ref to Tour}
   localField: '_id'
 });
 
-// PRE MIDDLEWARE: runs before save and .create() command but not .insertMany()
+// PRE MIDDLEWARE
 
 tourSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
-// To embedded data of guides of given id's to our guides array (But we're not gonna use it)
-// tourSchema.pre("save" , async function(next) {
-//   const guidesPromises = this.guides.map(async id => await User.findById(id));  // guidePromises store array of promises
-//   this.guides = await Promise.all(guidesPromises);
-//   console.log(this.guides);
-//   next();
-// })
 
-// tourSchema.post("save", function (doc, next) {
-//   // .> doc ->document just saved to db
-//   // console.log(doc);
-//   next();
-// });
+// QUERY MIDDLEWARE:
 
-// QUERY MIDDLEWARE: run before or after a certain query performed
-
-//tourSchema.pre('find' , function(next) {   // >> but if we req in get tour , we get it
 tourSchema.pre(/^find/, function (next) {
   // >> to avoid that ^ we use this
   this.find({ secretTour: { $ne: true } });
@@ -188,12 +167,6 @@ tourSchema.post(/^find/, function (doc, next) {
 
 
 //  AGGREGATION MIDDLEWARE
-
-// tourSchema.pre("aggregate", function (next) {
-//   // console.log(this);
-//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-//   next();
-// });
 
 const Tour = mongoose.model("Tour", tourSchema);
 
